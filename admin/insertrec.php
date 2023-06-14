85e2e5c5d00a2d1d2e01f0a5719f0d9b17aa1a83
Ragheil <Ragheil123@gmail.com>
2023-06-15 00:57:16 +0700

    hospital

--- admin/insertrec.php
+++ admin/insertrec.php
@@ -16,7 +16,6 @@ $entry_date= $_REQUEST['entry_date'];
 $refer_doctor= $_REQUEST['refer_doctor'];
 $diagnosis= $_REQUEST['diagnosis'];
 $department_name= $_REQUEST['department_name'];
-echo "Here comes the bride ". $patient_no . " ". $patient_name ." ". $age ." ". $sex ." ". $address ." ". $city ." ". $phone_number ." ". $entry_date ." " . $refer_doctor . " " . $diagnosis . " " . "$department_name"  ;
 
 $sql ="INSERT INTO pat_entry (patient_no,patient_name, age, sex, address, city, phone_number, entry_date, refer_doctor,diagnosis, department_name) VALUES ('$patient_no','$patient_name','$age','$sex','$address','$city','$phone_number','$entry_date','$refer_doctor','$diagnosis','$department_name')";
 if(mysqli_query($conn,$sql)){
